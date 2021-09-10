238bf2eb2a26b55e3e312d2cafeb29dac0e9f44c
Syed Mahbub <xsmahmed776@gmail.com>
2021-09-10 22:40:25 +0600

    s

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,18 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="container-fluid p-0 ps-4 pt-3 bg-dark text-white border-top ">
-            <select className="form-select bg-dark text-white text-center bi bi-globe mb-2" name="language" id="" style={{
-                width: "175px"
-            }}>
+            <div className="form-select ps-4 bg-dark m-0 mb-2" style={{width: "175px"}}>
+                <span className="bi bi-globe text-white"></span>
+            <select className="form-select bg-dark d-inline text-white text-center p-0" name="language" id="" style={{
+                width: "80%",
+                border: 0,
+                outline: 0,
+            }}> 
                 <option value="Us" className=""> English </option>
                 <option value="fr" className=""> French </option>
                 <option value="bn" className=""> Bangla </option>
             </select>
+            </div>
             <ul className="nav justify-content-between flex-column navbar-dark bg-dark w-100 ps-0">
                 <li className="nav-item">
                     <a href="#" className="nav-link text-white ps-0">
--- src/components/Header.js
+++ src/components/Header.js
@@ -160,16 +160,16 @@ const Header = () => {
     ]
 
     const handleClick = e => {
-        document.getElementById('mainCat').classList.add("d-none");
+        // document.getElementById('mainCat').style.visibility= "hidden";
+        document.getElementById('mainCat').style.transform= "translate(-100%)";
         document.getElementById(e.target.dataset.mainId).classList.add("subCatShow");
-        document.getElementById(e.target.dataset.mainId).classList.remove("d-none");
         document.getElementById(e.target.dataset.mainId).style.visibility= "visible";
         // console.log(e.target.dataset)
         
     }
     const backToCat = e => {
-        document.getElementById(e.target.dataset.mainId).classList.add("d-none");
-        document.getElementById('mainCat').classList.remove("d-none");
+        // document.getElementById('mainCat').style.visibility= "visible";
+        document.getElementById('mainCat').style.transform= "none";
         document.getElementById(e.target.dataset.mainId).classList.remove("subCatShow");
         document.getElementById(e.target.dataset.mainId).style.visibility= "hidden";
         // console.log(e.target.dataset)
@@ -195,52 +195,61 @@ const Header = () => {
             <a href="/"  className="navbar-brand me-0">VideoTube</a>
 
             <div className="offcanvas offcanvas-start nav-offcanvas" id="offcanvasCollapse" tabIndex="-1" aria-labelledby="offcanvasCollapseLabel">
-                
                 <div className="bg-white rounded-pill offcanvas-close">
-                <button className="btn-close text-dark" data-bs-dismiss="offcanvas" aria-label="Close" ></button>
+                    <button className="btn-close text-dark" data-bs-dismiss="offcanvas" aria-label="Close" ></button>
                 </div>
-                <ul className="navbar-nav ps-3 py-3" id="mainCat">
-                    {navbar.map(i=> (
-                        <li className="nav-item">
-                            <h6 className="nav-text">{i['category name']}</h6>
-                            <hr />
-                            <ul className="ps-2">
-                                {i.items.map(x=> ( 
-                            <>
-                                    <a 
-                                        className="nav-link" 
-                                        role="button"
-                                        onClick={e => handleClick(e)}                      
-                                        data-main-id={x.id}>
-                                            {x.name}
-                                    </a>
-                                    
-                                    
-                            </>
-                                ))}
-                            </ul>
-                        </li>
-                    ))}
-                </ul>
-            
-                {navbar.map(i=> (
-                    <>
-                        {i.items.map(x=> (
-                            <ul className="subCat d-none navbar-nav navbar-light px-3" id={x.id} >
-                                <button type="button" data-main-id={x.id} className="btn p-0 w-25 mt-2" onClick={e => backToCat(e)}>
-                                    <span className="bi bi-chevron-left w-100" style={{pointerEvents: "none"}}></span>
-                                </button>
-                                {x.items.map(z=> (
-                                    <li className="nav-item">
-                                        <a className="nav-link" role="button">{z}</a>
-                                    </li>
-                                ))}
-                            </ul>
+                <div className="overflow-hidden w-100 h-100 position-relative">
+                    <ul className="navbar-nav ps-3 py-3 mainCat" id="mainCat">
+                        <div className="d-flex justify-content-center flex-column border-bottom py-3 mb-2">
+                            <a href="#" className="nav-link text-primary">Login</a>
+                            <a href="#" className="nav-link text-primary">Create Account</a>
+                        </div>
+                        {navbar.map(i=> (
+                            <li className="nav-item">
+                                <h6 className="nav-text">{i['category name']}</h6>
+                                <hr />
+                                <ul className="ps-2">
+                                    {i.items.map(x=> ( 
+                                <>
+                                        <a 
+                                            className="nav-link d-flex justify-content-between" 
+                                            role="button"
+                                            onClick={e => handleClick(e)}                      
+                                            data-main-id={x.id}>
+                                                <span className="" style={{pointerEvents: "none"}} >{x.name}</span>
+                                                <span className="bi bi-chevron-right text-dark " style={{pointerEvents: "none"}} ></span>
+                                        </a>
+                                        
+                                        
+                                </>
+                                    ))}
+                                </ul>
+                            </li>
                         ))}
-                    
-                    </>
-                ))}                                 
-                                            
+                    </ul>
+                
+                    {navbar.map(i=> (
+                        <>
+                            {i.items.map(x=> (
+                                <ul className="subCat navbar-nav navbar-light px-3" id={x.id} >
+                                    <div className="d-flex w-100 border-bottom">
+                                        <button type="button" data-main-id={x.id} className="btn p-0 w-100 mt-2" onClick={e => backToCat(e)}>
+                                            <span className="bi bi-caret-left" style={{pointerEvents: "none"}}></span> Menu
+                                        </button>
+                                    </div>
+                                    
+
+                                    {x.items.map(z=> (
+                                        <li className="nav-item">
+                                            <a className="nav-link" role="button">{z}</a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ))}
+                        
+                        </>
+                    ))}                                 
+                </div>                                
             </div>        
 
             <div className="d-flex justify-content-evenly">
